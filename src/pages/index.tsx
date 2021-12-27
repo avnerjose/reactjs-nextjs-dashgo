ad14757f66371c0245244b17713726976c5ec746
avnerjose <avnerj.g.r@gmail.com>
2021-12-27 15:51:37 -0300

    Configuring chakra-ui, creating signin page and components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
 import type { NextPage } from "next";
+import { Flex, Button, Stack } from "@chakra-ui/react";
+import { Input } from "../components/Form/input";
 
-const Home: NextPage = () => {
-  return <h1>Home</h1>;
+const SignIn: NextPage = () => {
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p={8}
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input label="E-mail" name="email" type="email" />
+          <Input label="Senha" name="password" type="password" />
+          <Button type="submit" colorScheme="pink" size="lg">
+            Entrar
+          </Button>
+        </Stack>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Home;
